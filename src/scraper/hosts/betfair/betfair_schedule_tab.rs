045e6a2e671a7758bed626a46d8ca689a67e18ec
JoshuaXOng <joshuaong2000@gmail.com>
2022-09-17 08:49:43 +1000

    removed unsused import and commented code

--- src/scraper/hosts/betfair/betfair_schedule_tab.rs
+++ src/scraper/hosts/betfair/betfair_schedule_tab.rs
@@ -1,5 +1,4 @@
 use chrono::Timelike;
-use serde_json::de;
 
 use super::{betfair_tab::{BetfairTab, AsTab}, betfair_constants::{BETFAIR_CONSTANTS, BETFAIR_CSS_CONSTANTS}};
 use crate::{tabs::schedule_tab::{ScheduleTab, Event, AsScheduleTab, ScheduleTabError}};
@@ -10,52 +9,6 @@ impl AsScheduleTab for BetfairTab {
   }
 
   fn scrape_schedule(&self) -> Result<Vec<Event>, ScheduleTabError> {
-    // self.goto_url(BETFAIR_CONSTANTS.racing_url)
-    //   .map_err(|_| ScheduleTabError::BadScrape)?;
-
-    // let schedule_tabs = self.get_tab().tab_engine
-    //   .wait_for_elements(format!(".{}", BETFAIR_CSS_CONSTANTS.schedule_tab_class).as_str())
-    //   .map_err(|_| ScheduleTabError::BadScrape)?;
-    
-    // let browser_datetime = self.get_datetime()
-    //   .map_err(|_| ScheduleTabError::BadScrape)?;
-      
-    // let mut event_details: Vec<Event> = vec![];
-
-    // schedule_tabs.iter().try_for_each(|schedule_tab| -> Result<(), ScheduleTabError> {
-    //   schedule_tab.click().map_err(|_| ScheduleTabError::BadScrape)?;
-      
-    //   let venue_schedules = self.get_tab().tab_engine.wait_for_elements(
-    //     format!(".{}", BETFAIR_CSS_CONSTANTS.venue_schedule_class).as_str()
-    //   ).map_err(|_| ScheduleTabError::BadScrape)?;
-        
-    //   venue_schedules.iter().for_each(|venue_schedule| {
-    //     if let Ok(venue_schedule) = venue_schedule.get_inner_text() {
-    //       let venue_schedule_parts = venue_schedule.split("\n").collect::<Vec<&str>>();
-          
-    //       let venue_name = venue_schedule_parts.get(0);
-    //       let event_times = &venue_schedule_parts[1..];
-          
-    //       if let Some(venue_name) = venue_name {
-    //         event_times.iter().for_each(|event_time| {
-    //           let hour_and_min = event_time.split(":").collect::<Vec<&str>>();
-    //           let hour = hour_and_min.get(0).map(|hour| hour.parse::<u32>());
-    //           let min = hour_and_min.get(1).map(|min| min.parse::<u32>());
-    //           if let (Some(Ok(hour)), Some(Ok(min))) = (hour, min) {
-    //             if let Some(browser_datetime) = browser_datetime.clone().with_hour(hour).and_then(|browser_datetime| browser_datetime.with_minute(min)) {
-    //               event_details.push(Event {
-    //                 venue_name: String::from(*venue_name),
-    //                 planned_start_time: browser_datetime,
-    //                 has_started: true,
-    //               })
-    //             }
-    //           }
-    //         })
-    //       }
-    //     }
-    //   });
-
-    //   Ok(())
     let mut event_details: Vec<Event> = vec![];
 
     let nav_result = self.goto_url(BETFAIR_CONSTANTS.racing_url);
