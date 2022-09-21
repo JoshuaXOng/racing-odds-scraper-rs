eca25f88c909dc5948c939449dcc746b98e86f85
JoshuaXOng <joshuaong2000@gmail.com>
2022-09-21 21:09:07 +1000

    added multi threading example yo

--- src/scraper/main.rs
+++ src/scraper/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, thread, sync::Arc};
 
 use browser::{Browser, TabType, Host};
 use chrono::DateTime;
@@ -13,14 +13,30 @@ mod browser;
 mod extensions;
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let mut main_browser = Browser::new()?;
-  main_browser.open_page((TabType::Events, Host::Betfair))?;
-
-  if let Some(events_tabs) = main_browser.events_tabs.get(&Host::Betfair) {
-    if let Ok(odds) = events_tabs.scrape_event() {
-      println!("{:#?}", &odds);
+  let handler = thread::spawn(|| {
+    let mut main_browser = Browser::new().unwrap();
+    main_browser.open_page((TabType::Events, Host::Betfair)).unwrap();
+  
+    if let Some(events_tabs) = main_browser.events_tabs.get(&Host::Betfair) {
+      if let Ok(odds) = events_tabs.scrape_event() {
+        println!("{:#?}", &odds);
+      }
     }
-  }
+  });
+
+  let handler2 = thread::spawn(|| {
+    let mut main_browser = Browser::new().unwrap();
+    main_browser.open_page((TabType::Events, Host::Betfair)).unwrap();
+  
+    if let Some(events_tabs) = main_browser.events_tabs.get(&Host::Betfair) {
+      if let Ok(odds) = events_tabs.scrape_event() {
+        println!("{:#?}", &odds);
+      }
+    }
+  });
+
+  handler.join().unwrap();
+  handler2.join().unwrap();
   
   Ok(())
 }
