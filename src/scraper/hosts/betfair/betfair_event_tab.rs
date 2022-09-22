204cc1f5fdbe160064da4a63cf2e964ea0b81aed
JoshuaXOng <joshuaong2000@gmail.com>
2022-09-22 19:49:05 +1000

    proofed betfair event tab

--- src/scraper/hosts/betfair/betfair_event_tab.rs
+++ src/scraper/hosts/betfair/betfair_event_tab.rs
@@ -4,7 +4,7 @@ use chrono::{DateTime, FixedOffset, Duration};
 use headless_chrome::{Element, Tab as TabEngine};
 
 use super::{betfair_tab::{BetfairTab, AsTab, Tab, AsBetfairTab, ScheduleTab}, betfair_constants::{BETFAIR_CONSTANTS, BETFAIR_CSS_CONSTANTS}, betfair_schedule_tab::BetfairScheduleTab};
-use crate::{tabs::{events_tab::{EventsTab, AsEventsTab, ContestantOdds, EventsTabError}, schedule_tab::AsScheduleTab}, extensions::rhc_extensions::{for_each_node, is_node_of_class, create_element_from_bnid}};
+use crate::{tabs::{events_tab::{EventsTab, AsEventsTab, ContestantOdds, EventsTabError}, schedule_tab::AsScheduleTab}, extensions::rhc_extensions::{for_each_node, is_node_of_class, create_element_from_bnid}, models::{odds::Odds, money::Money}};
 
 pub struct BetfairEventsTab {
   pub betfair_tab: BetfairTab,
@@ -60,11 +60,44 @@ impl AsEventsTab for BetfairEventsTab {
       let c_entry_node = if let Ok(c_entry_node) = contestant_entry.get_description()
       { c_entry_node.to_owned() } else { continue };
 
+      let mut contestant_name = None as Option<String>;
+      let mut back_entries = vec![] as Vec<Odds>;
+      let mut lay_entries = vec![] as Vec<Odds>;
       for_each_node(&c_entry_node, &mut |node| {
-        if is_node_of_class(node, "back-button") {
-          println!("{:?}", create_element_from_bnid(&self.get_tab().tab_engine, node.backend_node_id).unwrap().get_inner_text());
+        if is_node_of_class(node, "runner-name") {
+          contestant_name = create_element_from_bnid(&self.get_tab().tab_engine, node.backend_node_id)
+            .and_then(|a| a.get_inner_text().or(Err(())))
+            .ok()
+            .map(|contestant_name| contestant_name);
+        }
+
+        let is_back_button = is_node_of_class(node, "back-button");
+        let is_lay_button = is_node_of_class(node, "lay-button");
+        if is_back_button || is_lay_button {
+          let entry_text = if let Ok(entry_text) = create_element_from_bnid(&self.get_tab().tab_engine, node.backend_node_id)
+            .and_then(|entry| entry.get_inner_text().or(Err(()))) 
+          { entry_text } else { return };
+
+          let odds_and_money = entry_text.split_whitespace().collect::<Vec<_>>();
+          let back_odds = dbg!(odds_and_money.get(0));
+          let back_money = dbg!(odds_and_money.get(1));
+
+          if let (Some(&odds), Some(&money)) = (back_odds, back_money) {
+            let entry_container = if is_back_button { &mut back_entries } else { &mut lay_entries };
+            if let Ok(odds) = (odds, money).try_into() as Result<Odds, ()> {
+              entry_container.push(odds);
+            }
+          }
         }
       });
+
+      if let Some(contestant_name) = contestant_name {
+        contestant_odds.push(ContestantOdds {
+          contestant_name: contestant_name,
+          backing_odds: back_entries,
+          laying_odds: lay_entries
+        })
+      }
     }
 
     Ok(contestant_odds)
--- src/scraper/models/odds.rs
+++ src/scraper/models/odds.rs
@@ -5,3 +5,15 @@ pub enum Odds {
   OddsWoLiquidity(f64),
   OddsWLiquidity(f64, Money),
 }
+
+impl TryFrom<(&str, &str)> for Odds {
+  type Error = ();
+
+  fn try_from((odds, money): (&str, &str)) -> Result<Self, Self::Error> {
+    if let (Ok(odds), Ok(money)) = (odds.parse::<f64>(), money.replace("$", "").parse::<f64>()) {
+      Ok(Self::OddsWLiquidity(odds, Money::AUS(money)))
+    } else {
+      Err(())
+    }
+  }
+}
